2d1af2be4dfd8464305251db9b6eb4727a6fd378
Sheikh-hadi <sheikhgee322@gmail.com>
2024-07-24 20:17:40 +0500

    ++

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,36 +5,48 @@ import './Header.css';
 
 const Header = () => {
     return (
-        <header style={{ border: "2px solid black" }} className='header'>
-            <Row style={{ border: "2px solid black" }} align={'middle'}>
-                <Col style={{ border: "2px solid black" }} xs={12} sm={12} md={12}>
+        <header  className='header'>
+            <Row  align={'middle'}>
+                <Col  xs={8} sm={10} md={10}>
                     <Image
                         src={headerLogo}
                     />
                 </Col>
-                <Col style={{ border: "2px solid black" }} xs={12} sm={12} md={12}>
-                <Row>
-                    <ul style={{ border: "2px solid black" }} className="nav-bar-ul">
-                        <li style={{ border: "2px solid black" }} className="nav-bar-li">
-                            <a style={{ border: "2px solid black" }} href="/" className="nav-bar-anchor">Home</a>
-                        </li>
-                        <li style={{ border: "2px solid black" }} className="nav-bar-li">
-                            <a style={{ border: "2px solid black" }} href="/about" className="nav-bar-anchor">About Us</a>
-                        </li>
-                        <li style={{ border: "2px solid black" }} className="nav-bar-li">
-                            <a style={{ border: "2px solid black" }} href="#" className="nav-bar-anchor">Instagram</a>
-                        </li>
-                        <li style={{ border: "2px solid black" }} className="nav-bar-li">
-                            <a style={{ border: "2px solid black" }} href="/" className="nav-bar-anchor">Tiktok</a>
-                        </li>
-                        <li style={{ border: "2px solid black" }} className="nav-bar-li">
-                            <a style={{ border: "2px solid black" }} href="/blog" className="nav-bar-anchor">Blog</a>
-                        </li>
-                        <li style={{ border: "2px solid black" }} className="nav-bar-li">
-                            <a style={{ border: "2px solid black" }} href="/contact" className="nav-bar-anchor">Contact Us</a>
-                        </li>
-                    </ul>
+                <Col xs={16} sm={14} md={14}>
+                    {/* <ul  className="nav-bar-ul"> */}
+                    <Row gutter={[50, 24]} align={'middle'}>
+                        <Col xs={4} sm={4} md={4}>
+                            <li  className="nav-bar-li">
+                                <a  href="/" className="nav-bar-anchor">Home</a>
+                            </li>
+                        </Col>
+                        <Col xs={4} sm={4} md={4}>
+                            <li  className="nav-bar-li">
+                                <a  href="/about" className="nav-bar-anchor">About Us</a>
+                            </li>
+                        </Col>
+                        <Col xs={4} sm={4} md={4}>
+                            <li  className="nav-bar-li">
+                                <a  href="#" className="nav-bar-anchor">Instagram</a>
+                            </li>
+                        </Col>
+                        <Col xs={4} sm={4} md={4}>
+                            <li  className="nav-bar-li">
+                                <a  href="/" className="nav-bar-anchor">Tiktok</a>
+                            </li>
+                        </Col >
+                        <Col xs={4} sm={4} md={4}>
+                            <li  className="nav-bar-li">
+                                <a  href="/blog" className="nav-bar-anchor">Blog</a>
+                            </li>
+                        </Col>
+                        <Col >
+                            <li  className="nav-bar-li">
+                                <a  href="/contact" className="nav-bar-anchor">Contact Us</a>
+                            </li>
+                        </Col>
                     </Row>
+                    {/* </ul> */}
                 </Col>
             </Row>
         </header>
